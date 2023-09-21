232c6c68a4e687a544475c90849dc699577fb47b
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-09-21 11:10:26 +0300

    upd

--- partials/footer.php
+++ partials/footer.php
@@ -34,6 +34,25 @@
           Настоящий ресурс содержит материалы категории <span>18+</span>
         </div>
         <div class="footer-secondary__copyright">© OleoScope 2023</div>
+        <div class="footer-secondary__counters">
+          <!-- Yandex.Metrika counter -->
+          <script type="text/javascript" >
+             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+             m[i].l=1*new Date();
+             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+             ym(55243105, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+             });
+          </script>
+          <noscript><div><img src="https://mc.yandex.ru/watch/55243105" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <!-- /Yandex.Metrika counter -->
+        </div>
       </div>
     </div>
   </div>
