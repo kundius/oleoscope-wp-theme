ab968cce7788d0a858d6fd0faffa90b210745ddd
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-11-02 01:04:30 +0300

    Update widget-hot.php

--- partials/widget-hot.php
+++ partials/widget-hot.php
@@ -7,7 +7,7 @@ $partners_query = new WP_Query([
   'post_type' => 'news',
   'posts_per_page' => 5,
   'order' => 'DESC',
-  'meta_key' => 'views',
+  'meta_key' => 'post_views_count',
   'orderby' => 'meta_value_num',
   'date_query' => [
     [
